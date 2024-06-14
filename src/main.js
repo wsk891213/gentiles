626645fff1cd11c568f47d558270496fe09aa5b0
DESKTOP-5I71T5K\관리자 <wsk891213@gmail.com>
2024-06-14 15:50:55 +0900

    'bootstrap/dist/css/bootstrap.css'  중복현상으로 제거

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router  from './router'
 import BootstrapVue3 from 'bootstrap-vue-3'
 
-import 'bootstrap/dist/css/bootstrap.css'
+//import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 const app = createApp(App)
