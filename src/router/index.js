db342bda2fbbe54876089d40826b6848d76a432f
jonny <jonnygim.dev@gmail.com>
2024-06-28 21:51:16 +0900

    router 이동시 화면 최상단으로 이동

--- src/router/index.js
+++ src/router/index.js
@@ -26,7 +26,10 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
+  scrollBehavior() {
+    return { top: 0 };
+  }
 })
 
 export default router
